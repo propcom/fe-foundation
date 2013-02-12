4690bb02d4abb87a44b151d3ccb7053b0c564abe
Stephen Nolan <stephen.nolan@propcom.co.uk>
2013-02-12 16:56:00 +0000

    style guide work

--- fuel/app/views/contact.php
+++
@@ -1,2 +0,0 @@
-<h1>This does be the Contact page!</h1>
-<article id="google-map">&nbsp;</article>
--- fuel/app/views/index.php
+++ fuel/app/views/index.php
@@ -1,15 +1,4 @@
-<p>1. Run sass from the assets Directory</p>
+<p>Run sass from the assets Directory</p>
 <pre>sass --watch sass/:css</pre>
-
-<p>2. Style guide</p>
-
-<p>3. Plugins</p>
-	<ul>
-		<li>modernizr</li>
-		<li>conditionizr</li>
-		<li>skeleton</li>
-		<li>sass</li>
-	</ul>
-
-<p>4. SCSS Organization</p>
-	
+<p>Then Build all the things. <a href="style-guide">Style guide</a> first</p>
+<p>Change the favicon. Or Dave will kill you!</p>
