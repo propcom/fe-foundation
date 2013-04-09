3cb025cb1483db3b117c5f1499ecbadf9ac51770
Stephen Nolan <stephen.nolan@propcom.co.uk>
2013-04-09 09:48:31 +0100

    content on index

--- fuel/app/views/index.php
+++ fuel/app/views/index.php
@@ -2,3 +2,13 @@
 <pre>sass --watch sass/:css</pre>
 <p>Then Build all the things. <a href="style-guide">Style guide</a> first</p>
 <p>Change the favicon. Or Dave will kill you!</p>
+
+<p>What's installed:</p>
+<ul>
+	<li>SASS</li>
+	<li>Skeleton</li>
+	<li>Normalise</li>
+	<li>Modernizr</li>
+	<li>Bourbon</li>
+</ul>
+
