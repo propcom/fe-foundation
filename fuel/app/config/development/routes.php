27b165810aec250710eb1fd5de5c64be129f9d31
Andrew Wheal <andreww@propcom.co.uk>
2013-05-02 11:39:13 +0100

    routes for code-guide to make it 404 when not in development mode

---
+++ fuel/app/config/development/routes.php
@@ -0,0 +1,5 @@
+<?php
+
+return array(
+	'code-guide' => 'static/index',
+);
--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,11 +1,7 @@
 <?php
 
 return array(
-	//'_root_' => 'static/index', // The default route
-	//'_404_' => 'static/index', // The main 404 route
+	'_root_' => 'static/index', // The default route
+	'_404_' => 'static/index', // The main 404 route
 	'code-guide' => 'static/404',
-		/* '(:any)' => function() {
-		  var_dump(func_get_args());
-		  die;
-		  }, */
 );
