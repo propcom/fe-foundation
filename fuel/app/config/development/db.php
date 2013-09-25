cce3392a3b385894e937fdfb98b1310ed14dc9e0
Alastair McGowan-Douglas <alastair@propcom.co.uk>
2013-09-25 11:40:03 +0100

    Create skeleton DB config, and force people to edit it

---
+++ fuel/app/config/development/db.php
@@ -0,0 +1,12 @@
+<?php
+
+// remove these lines when you've done it.
+die("Set up your DB in fuel/app/config/development/db.php!");
+
+return [
+	[
+		'dsn' => 'mysql:host=localhost;dbname=dbname',
+		'username' => 'username',
+		'password' => 'password',
+	],
+];
