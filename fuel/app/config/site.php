068bbcf9213e4d8ce5f89c89fe4b28bd333845c6
Brett Chisholm <brett.chisholm@propcom.co.uk>
2014-05-08 12:12:04 +0100

    [Config] - create new config files to hold new global variables

---
+++ fuel/app/config/site.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.0
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2012 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+return array (
+
+	'site_email' => 'brett.chisholm@propcom.co.uk',
+
+	'phone' => '077777777777',
+
+	'analytics' => 'UA-XXXXX-X',
+
+	'siteid' => '123',
+
+	'sitename' => "Brett's - awesome site",
+
+	'twitter' => "PropellerComms"
+
+);
